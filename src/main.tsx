b43052edc2a21e0304dfbc73a765556a701441d3
paper-rockets <majorrichardpsn@gmail.com>
2026-09-01 18:31:36 -0400

    Clean build with direct App mount

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { ErrorBoundary } from './components/ErrorBoundary.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
+  <App />
 );
